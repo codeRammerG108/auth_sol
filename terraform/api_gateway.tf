ac482bf83c7ce8858e467b8a87b850a125a6021e
Nishanth Huligeri <Nishanth.Huligeri@triconinfotech.com>
2024-01-22 18:49:48 +0530

    add111

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -27,7 +27,7 @@ resource "aws_api_gateway_integration" "integration" {
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda.invoke_arn
+  uri                     = aws_lambda_function.add_cognito_user.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "composable_auth_api_gateway_deployment" {
