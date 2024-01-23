49ce47eb2d0c61258d4ee08ee8b672c95cb82030
codeRammerG108 <Abhishek.Sandliya@triconinfotech.com>
2024-01-23 11:24:58 +0530

    formatted

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,26 +1,26 @@
 # Variables
 # variable "myregion" {}
- 
+
 # variable "accountId" {}
- 
+
 # API Gateway
 resource "aws_api_gateway_rest_api" "composable_auth_api_gateway" {
   name = "${var.project_name}_composable_auth_api_gateway"
 }
- 
+
 resource "aws_api_gateway_resource" "resource" {
   path_part   = "add-user"
   parent_id   = aws_api_gateway_rest_api.composable_auth_api_gateway.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.composable_auth_api_gateway.id
 }
- 
+
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.composable_auth_api_gateway.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
- 
+
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.composable_auth_api_gateway.id
   resource_id             = aws_api_gateway_resource.resource.id
@@ -29,20 +29,20 @@ resource "aws_api_gateway_integration" "integration" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.add_cognito_user.invoke_arn
 }
- 
+
 resource "aws_api_gateway_deployment" "composable_auth_api_gateway_deployment" {
   depends_on  = [aws_api_gateway_rest_api.composable_auth_api_gateway]
   rest_api_id = aws_api_gateway_rest_api.composable_auth_api_gateway.id
- 
+
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.composable_auth_api_gateway.body))
   }
- 
+
   lifecycle {
     create_before_destroy = true
   }
 }
- 
+
 resource "aws_api_gateway_stage" "example" {
   deployment_id = aws_api_gateway_deployment.composable_auth_api_gateway_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.composable_auth_api_gateway.id
